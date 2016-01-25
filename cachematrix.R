9b6d115ca95ebae5f69eafcf0b1d1df92d8e0013
chiu <chiucode@gmail.com>
2016-01-24 17:47:06 -0800

    both functions cachesolve and makecachematrix completed, implemented testsuite called huge_matrix to test time difference.

--- cachematrix.R
+++ cachematrix.R
@@ -5,35 +5,35 @@
 # This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-  # browser()
-  # inverse_result <- NULL
   inverse_result <- NULL  # a dummy variable for caching the result
-  set <- function(y){
+  set <- function(y) {
     x <<- y
     inverse_result <<- NULL
   }
-  get <- function() x
-  SetInverseResult <- function(inverse_result_arg){
+  get <- function(){
+    return(x)
+  }
+  SetInverseResult <- function(inverse_result_arg) {
     inverse_result <<- inverse_result_arg
   }
-  GetInverseResult <- function(){
+  GetInverseResult <- function() {
     return(inverse_result)
   }
-  result <- list(set = set, get = get, SetInverseResult = SetInverseResult, GetInverseResult = GetInverseResult)
+  result <-
+    list(
+      set = set, get = get, SetInverseResult = SetInverseResult, GetInverseResult = GetInverseResult
+    )
   return(result)
-  
-  
 }
 
 
 ## Write a short comment describing this function
-
+# This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then cacheSolve should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
-  
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
   inverse_result <- x$GetInverseResult()
   
-  if(!is.null(inverse_result)){
+  if (!is.null(inverse_result)) {
     message('getting cached data')
     return(inverse_result)
   }
@@ -43,17 +43,17 @@ cacheSolve <- function(x, ...) {
   return(inverse_result)
 }
 
-some_matrix = matrix( c(2, 4, 3, 1, 5, 7, 1 , 2, 3), nrow=3,   ncol=3) 
-
-matrix_functions <- makeCacheMatrix(some_matrix)
-time_start <- Sys.time()
-cache_solve_output <- cacheSolve(matrix_functions)
-time_end <- Sys.time()
-time_diff <- time_end - time_start
-print(paste0('for non caching; time diff is:', time_diff))
-
-time_start <- Sys.time()
-cache_solve_output <- cacheSolve(matrix_functions)
-time_end <- Sys.time()
-time_diff <- time_end - time_start
-print(paste0('for caching time diff is:', time_diff))
+# some_matrix = matrix( c(2, 4, 3, 1, 5, 7, 1 , 2, 3), nrow=3,   ncol=3)
+# 
+# matrix_functions <- makeCacheMatrix(some_matrix)
+# time_start <- Sys.time()
+# cache_solve_output <- cacheSolve(matrix_functions)
+# time_end <- Sys.time()
+# time_diff <- time_end - time_start
+# print(paste0('for non caching; time diff is:', time_diff))
+# 
+# time_start <- Sys.time()
+# cache_solve_output <- cacheSolve(matrix_functions)
+# time_end <- Sys.time()
+# time_diff <- time_end - time_start
+# print(paste0('for caching time diff is:', time_diff))
--- huge_matrix.R
+++ huge_matrix.R
@@ -12,8 +12,17 @@ dat <- mvrnorm(10000, mu=rep(0,k), Sigma=S) ### be patient!
 R <- cor(dat)
 
 matrix_functions <- makeCacheMatrix(R)
+time_start <- Sys.time()
 system.time(result1 <- cacheSolve(matrix_functions))
+time_end <- Sys.time()
+time_diff <- time_end - time_start
+print(paste0('for non caching; time diff is:', time_diff))
+
+time_start <- Sys.time()
 system.time(result2 <- cacheSolve(matrix_functions))
+time_end <- Sys.time()
+time_diff <- time_end - time_start
+print(paste0('for caching; time diff is:', time_diff))
 
 # system.time(RI1 <- solve(R))
 # system.time(RI2 <- chol2inv(chol(R)))
