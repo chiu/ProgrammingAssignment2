a61a9d1da83019e08193401efa91796f16a04151
chiu <chiucode@gmail.com>
2016-01-24 16:35:29 -0800

    Starting matrix code.

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,26 @@
 ## functions do
 
 ## Write a short comment describing this function
+# This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  browser()
+  inverse_result <- NULL  # a dummy variable for caching the result
+  set <- function(y){
+    x <<- y
+    inverse_result <<- NULL
+  }
+  get <- function() x
+  SetInverseResult <- function(inverse_result_arg){
+    inverse_result <<- inverse_result_arg
+  }
+  GetInverseResult <- function(){
+    return(inverse_result)
+  }
+  list(set = set, get = get, SetInverseResult = SetInverseResult, GetInverseResult = GetInverseResult)
+  
+  
+  
 }
 
 
@@ -13,3 +30,5 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+
+some_matrix = matrix( c(2, 4, 3, 1, 5, 7), nrow=3,   ncol=2) 
